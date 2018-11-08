6318f045c8e400981bdaab578325db8f6a3a35ba
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-11-08 22:22:26 +0800

    Use SwipeRefreshLayout

--- app/src/main/java/org/fbluemle/testcoinlist/ui/MainActivity.java
+++ app/src/main/java/org/fbluemle/testcoinlist/ui/MainActivity.java
@@ -4,6 +4,7 @@ import org.fbluemle.testcoinlist.R;
 import org.fbluemle.testcoinlist.data.TickerRow;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    private SwipeRefreshLayout mSwipeRefreshLayout;
     private TickerAdapter mTickerAdapter;
 
     @Override
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         mTickerAdapter = new TickerAdapter();
 
+        mSwipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
+        mSwipeRefreshLayout.setOnRefreshListener(this::refreshTickerItems);
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -34,7 +38,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshTickerItems() {
-        mTickerAdapter.setItems(Arrays.asList(getTickerItems()));
+        mSwipeRefreshLayout.setRefreshing(true);
+        TickerRow[] items = getTickerItems();
+        mTickerAdapter.setItems(Arrays.asList(items));
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 
     private TickerRow[] getTickerItems() {
