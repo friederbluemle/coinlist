aec71db7bab0aa7c918987aabc7c6823919ec9aa
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-11-08 20:02:59 +0800

    Add Ticker data model class and RecyclerView

---
+++ app/src/main/java/org/fbluemle/testcoinlist/data/TickerRow.java
@@ -0,0 +1,9 @@
+package org.fbluemle.testcoinlist.data;
+
+public class TickerRow {
+    public int id;
+    public String name;
+    public String symbol;
+    public int rank;
+    public float price;
+}
