331176bb02e56272c9622fb5d5f02d98a45f98e5
Frieder Bluemle <frieder.bluemle@gmail.com>
2018-11-08 23:16:12 +0800

    Make HTTP call using OkHttp

--- app/src/main/java/org/fbluemle/testcoinlist/DataSource.java
+++ app/src/main/java/org/fbluemle/testcoinlist/DataSource.java
@@ -2,7 +2,22 @@ package org.fbluemle.testcoinlist;
 
 import org.fbluemle.testcoinlist.data.TickerRow;
 
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
 public class DataSource {
+    private static final String URL = "https://api.coinmarketcap.com/v2/ticker/";
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+
+    public static Call callTicker() {
+        Request request = new Request.Builder()
+                .url(URL)
+                .build();
+
+        return CLIENT.newCall(request);
+    }
+
     public static TickerRow[] getTickerItems() {
         return new TickerRow[] {
                 new TickerRow(1, "Bitcoin", getRandomPrice()),
--- app/src/main/java/org/fbluemle/testcoinlist/ui/MainActivity.java
+++ app/src/main/java/org/fbluemle/testcoinlist/ui/MainActivity.java
@@ -9,8 +9,14 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import java.io.IOException;
 import java.util.Arrays;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     private SwipeRefreshLayout mSwipeRefreshLayout;
@@ -39,9 +45,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void refreshTickerItems() {
+        Log.d("MainActivity", "Refreshing ...");
+
         mSwipeRefreshLayout.setRefreshing(true);
         TickerRow[] items = DataSource.getTickerItems();
         mTickerAdapter.setItems(Arrays.asList(items));
         mSwipeRefreshLayout.setRefreshing(false);
+
+        DataSource.callTicker().enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("MainActivity", "Error", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("MainActivity", "From API: " + response.body().string());
+            }
+        });
     }
 }
